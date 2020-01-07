0bc4c414442fd1a20a885a78ef7d1fe76184d37c
a-glapinski <49104470+a-glapinski@users.noreply.github.com>
2020-01-07 17:36:53 +0100

    Coroutines

--- src/main/kotlin/pgm/PgmImage.kt
+++ src/main/kotlin/pgm/PgmImage.kt
@@ -1,5 +1,8 @@
 package pgm
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import java.io.File
 
 open class PgmImage protected constructor(protected val size: Int) {
@@ -37,16 +40,18 @@ open class PgmImage protected constructor(protected val size: Int) {
     fun convolute(n: Int) {
         val convolutedImage = Array(size) { DoubleArray(size) }
         repeat(n) {
-            (0 until size).toList().parallelStream().forEach { i ->
-                for (j in 0 until size) {
-                    var newValue = 0.6 * image[i][j]
+            (0 until size).chunked(if (size == 8) 1 else size / 16).forEachParallel {
+                for (i in it) {
+                    for (j in 0 until size) {
+                        var newValue = 0.6 * image[i][j]
 
-                    if (i != 0) newValue += 0.1 * image[i - 1][j]
-                    if (i != size - 1) newValue += 0.1 * image[i + 1][j]
-                    if (j != 0) newValue += 0.1 * image[i][j - 1]
-                    if (j != size - 1) newValue += 0.1 * image[i][j + 1]
+                        if (i != 0) newValue += 0.1 * image[i - 1][j]
+                        if (i != size - 1) newValue += 0.1 * image[i + 1][j]
+                        if (j != 0) newValue += 0.1 * image[i][j - 1]
+                        if (j != size - 1) newValue += 0.1 * image[i][j + 1]
 
-                    convolutedImage[i][j] = newValue
+                        convolutedImage[i][j] = newValue
+                    }
                 }
             }
             convolutedImage.copyInto(image)
@@ -72,4 +77,8 @@ open class PgmImage protected constructor(protected val size: Int) {
             }
         }
     }
+}
+
+private inline fun <T> Collection<T>.forEachParallel(crossinline action: suspend (T) -> Unit) = runBlocking {
+    map { async(Dispatchers.Default) { action(it) } }.forEach { it.await() }
 }
