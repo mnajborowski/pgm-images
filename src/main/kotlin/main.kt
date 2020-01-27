6e468c5d5195434810faa39b5c0411eeed00099b
a-glapinski <49104470+a-glapinski@users.noreply.github.com>
2020-01-27 21:30:28 +0100

    Non-parallel

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,51 +1,11 @@
-import pgm.*
+import pgm.Filters
+import pgm.PgmImage
 import kotlin.system.measureTimeMillis
 
 fun main() {
-    // Shapes
-    val chessboard = Chessboard(1024)
-    chessboard.saveToFile("chessboard.pgm", 255)
-
-    val tLetter = TLetter(64)
-    tLetter.saveToFile("t_letter.pgm", 255)
-
-    val verticalStripes = VerticalStripes(64)
-    verticalStripes.saveToFile("vertical_stripes.pgm", 255)
-
-    val horizontalStripes = HorizontalStripes(64)
-    horizontalStripes.saveToFile("horizontal_stripes.pgm", 255)
-
     // Filters
     val blur = PgmImage.loadFromFile("chessboard.pgm")
     val time = measureTimeMillis { blur.convolute(1000, Filters.blur) }
     println(time)
     blur.saveToFile("chessboard_blur.pgm", 255)
-
-    val boxBlur = PgmImage.loadFromFile("chessboard.pgm")
-    boxBlur.convolute(100, Filters.boxBlur)
-    boxBlur.saveToFile("chessboard_box_blur.pgm", 255)
-
-    val sharpen = PgmImage.loadFromFile("chessboard_blur.pgm")
-    sharpen.convolute(1, Filters.sharpen)
-    sharpen.saveToFile("chessboard_sharpen.pgm", 255)
-
-    val outline = PgmImage.loadFromFile("chessboard.pgm")
-    outline.convolute(1, Filters.outline)
-    outline.saveToFile("chessboard_outline.pgm", 255)
-
-    val emboss = PgmImage.loadFromFile("chessboard_blur.pgm")
-    emboss.convolute(1, Filters.emboss)
-    emboss.saveToFile("chessboard_emboss.pgm", 255)
-
-    val bottomSobel = PgmImage.loadFromFile("chessboard.pgm")
-    bottomSobel.convolute(1, Filters.bottomSobel)
-    bottomSobel.saveToFile("chessboard_bottom_sobel.pgm", 255)
-
-    val leftSobel = PgmImage.loadFromFile("chessboard.pgm")
-    leftSobel.convolute(1, Filters.leftSobel)
-    leftSobel.saveToFile("chessboard_left_sobel.pgm", 255)
-
-    val weird = PgmImage.loadFromFile("chessboard_blur.pgm")
-    weird.convolute(2, Filters.weird)
-    weird.saveToFile("chessboard_weird.pgm", 255)
 }
--- src/main/kotlin/pgm/Filter.kt
+++ src/main/kotlin/pgm/Filter.kt
@@ -1,8 +1,5 @@
 package pgm
 
-import utils.forEachParallel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 
 class Filter(private val kernel: Array<FloatArray>) {
@@ -15,86 +12,81 @@ class Filter(private val kernel: Array<FloatArray>) {
         val size = pgmImage.size
 
         val convolutedImage = Array(size) { FloatArray(size) }
-        val blocks = (1 until size - 1).chunked((size / 32).coerceAtLeast(1))
 
         repeat(n) {
-            val calculateBordersJob = async(Dispatchers.Default) {
-                var upperLeftCorner = kernel[1][1] * image[0][0]
-                upperLeftCorner += kernel[2][1] * image[1][0]
-                upperLeftCorner += kernel[1][2] * image[0][1]
-                upperLeftCorner += kernel[2][2] * image[1][1]
-                convolutedImage[0][0] = upperLeftCorner
+            var upperLeftCorner = kernel[1][1] * image[0][0]
+            upperLeftCorner += kernel[2][1] * image[1][0]
+            upperLeftCorner += kernel[1][2] * image[0][1]
+            upperLeftCorner += kernel[2][2] * image[1][1]
+            convolutedImage[0][0] = upperLeftCorner
 
-                var upperRightCorner = kernel[1][1] * image[0][size - 1]
-                upperRightCorner += kernel[1][0] * image[0][size - 2]
-                upperRightCorner += kernel[2][1] * image[1][size - 1]
-                upperRightCorner += kernel[1][1] * image[1][size - 2]
-                convolutedImage[0][size - 1] = upperRightCorner
+            var upperRightCorner = kernel[1][1] * image[0][size - 1]
+            upperRightCorner += kernel[1][0] * image[0][size - 2]
+            upperRightCorner += kernel[2][1] * image[1][size - 1]
+            upperRightCorner += kernel[1][1] * image[1][size - 2]
+            convolutedImage[0][size - 1] = upperRightCorner
 
-                var lowerLeftCorner = kernel[1][1] * image[size - 1][0]
-                lowerLeftCorner += kernel[0][1] * image[size - 2][0]
-                lowerLeftCorner += kernel[1][2] * image[size - 1][1]
-                lowerLeftCorner += kernel[0][2] * image[size - 2][1]
-                convolutedImage[size - 1][0] = lowerLeftCorner
+            var lowerLeftCorner = kernel[1][1] * image[size - 1][0]
+            lowerLeftCorner += kernel[0][1] * image[size - 2][0]
+            lowerLeftCorner += kernel[1][2] * image[size - 1][1]
+            lowerLeftCorner += kernel[0][2] * image[size - 2][1]
+            convolutedImage[size - 1][0] = lowerLeftCorner
 
-                var lowerRightCorner = kernel[1][1] * image[size - 1][size - 1]
-                lowerRightCorner += kernel[1][0] * image[size - 1][size - 2]
-                lowerRightCorner += kernel[0][1] * image[size - 2][size - 1]
-                lowerRightCorner += kernel[0][0] * image[size - 2][size - 2]
-                convolutedImage[size - 1][size - 1] = lowerRightCorner
+            var lowerRightCorner = kernel[1][1] * image[size - 1][size - 1]
+            lowerRightCorner += kernel[1][0] * image[size - 1][size - 2]
+            lowerRightCorner += kernel[0][1] * image[size - 2][size - 1]
+            lowerRightCorner += kernel[0][0] * image[size - 2][size - 2]
+            convolutedImage[size - 1][size - 1] = lowerRightCorner
 
-                for (index in 1 until size - 1) {
-                    var upperRowValue = kernel[1][1] * image[0][index]
-                    upperRowValue += kernel[1][0] * image[0][index - 1]
-                    upperRowValue += kernel[1][2] * image[0][index + 1]
-                    upperRowValue += kernel[2][0] * image[1][index - 1]
-                    upperRowValue += kernel[2][1] * image[1][index]
-                    upperRowValue += kernel[2][2] * image[1][index + 1]
-                    convolutedImage[0][index] = upperRowValue
+            for (index in 1 until size - 1) {
+                var upperRowValue = kernel[1][1] * image[0][index]
+                upperRowValue += kernel[1][0] * image[0][index - 1]
+                upperRowValue += kernel[1][2] * image[0][index + 1]
+                upperRowValue += kernel[2][0] * image[1][index - 1]
+                upperRowValue += kernel[2][1] * image[1][index]
+                upperRowValue += kernel[2][2] * image[1][index + 1]
+                convolutedImage[0][index] = upperRowValue
 
-                    var lowerRowValue = kernel[1][1] * image[size - 1][index]
-                    lowerRowValue += kernel[1][0] * image[size - 1][index - 1]
-                    lowerRowValue += kernel[1][2] * image[size - 1][index + 1]
-                    lowerRowValue += kernel[0][0] * image[size - 2][index - 1]
-                    lowerRowValue += kernel[0][1] * image[size - 2][index]
-                    lowerRowValue += kernel[0][2] * image[size - 2][index + 1]
-                    convolutedImage[size - 1][index] = lowerRowValue
+                var lowerRowValue = kernel[1][1] * image[size - 1][index]
+                lowerRowValue += kernel[1][0] * image[size - 1][index - 1]
+                lowerRowValue += kernel[1][2] * image[size - 1][index + 1]
+                lowerRowValue += kernel[0][0] * image[size - 2][index - 1]
+                lowerRowValue += kernel[0][1] * image[size - 2][index]
+                lowerRowValue += kernel[0][2] * image[size - 2][index + 1]
+                convolutedImage[size - 1][index] = lowerRowValue
 
-                    var leftColumnValue = kernel[1][1] * image[index][0]
-                    leftColumnValue += kernel[0][1] * image[index - 1][0]
-                    leftColumnValue += kernel[2][1] * image[index + 1][0]
-                    leftColumnValue += kernel[0][2] * image[index + 1][1]
-                    leftColumnValue += kernel[1][2] * image[index][1]
-                    leftColumnValue += kernel[2][2] * image[index - 1][1]
-                    convolutedImage[index][0] = leftColumnValue
+                var leftColumnValue = kernel[1][1] * image[index][0]
+                leftColumnValue += kernel[0][1] * image[index - 1][0]
+                leftColumnValue += kernel[2][1] * image[index + 1][0]
+                leftColumnValue += kernel[0][2] * image[index + 1][1]
+                leftColumnValue += kernel[1][2] * image[index][1]
+                leftColumnValue += kernel[2][2] * image[index - 1][1]
+                convolutedImage[index][0] = leftColumnValue
 
-                    var rightColumnValue = kernel[1][1] * image[index][size - 1]
-                    rightColumnValue += kernel[0][1] * image[index - 1][size - 1]
-                    rightColumnValue += kernel[2][1] * image[index + 1][size - 1]
-                    rightColumnValue += kernel[0][0] * image[index - 1][size - 2]
-                    rightColumnValue += kernel[1][0] * image[index][size - 2]
-                    rightColumnValue += kernel[2][0] * image[index + 1][size - 2]
-                    convolutedImage[index][size - 1] = rightColumnValue
+                var rightColumnValue = kernel[1][1] * image[index][size - 1]
+                rightColumnValue += kernel[0][1] * image[index - 1][size - 1]
+                rightColumnValue += kernel[2][1] * image[index + 1][size - 1]
+                rightColumnValue += kernel[0][0] * image[index - 1][size - 2]
+                rightColumnValue += kernel[1][0] * image[index][size - 2]
+                rightColumnValue += kernel[2][0] * image[index + 1][size - 2]
+                convolutedImage[index][size - 1] = rightColumnValue
+            }
+
+            for (i in 1 until size - 1) {
+                for (j in 1 until size - 1) {
+                    var newValue = kernel[1][1] * image[i][j]
+                    newValue += kernel[0][0] * image[i + 1][j - 1]
+                    newValue += kernel[0][1] * image[i + 1][j]
+                    newValue += kernel[0][2] * image[i + 1][j + 1]
+                    newValue += kernel[1][0] * image[i][j - 1]
+                    newValue += kernel[1][2] * image[i][j + 1]
+                    newValue += kernel[2][0] * image[i - 1][j - 1]
+                    newValue += kernel[2][1] * image[i - 1][j]
+                    newValue += kernel[2][2] * image[i - 1][j + 1]
+
+                    convolutedImage[i][j] = newValue
                 }
             }
-            blocks.forEachParallel {
-                for (i in it) {
-                    for (j in 1 until size - 1) {
-                        var newValue = kernel[1][1] * image[i][j]
-                        newValue += kernel[0][0] * image[i + 1][j - 1]
-                        newValue += kernel[0][1] * image[i + 1][j]
-                        newValue += kernel[0][2] * image[i + 1][j + 1]
-                        newValue += kernel[1][0] * image[i][j - 1]
-                        newValue += kernel[1][2] * image[i][j + 1]
-                        newValue += kernel[2][0] * image[i - 1][j - 1]
-                        newValue += kernel[2][1] * image[i - 1][j]
-                        newValue += kernel[2][2] * image[i - 1][j + 1]
-
-                        convolutedImage[i][j] = newValue
-                    }
-                }
-            }
-            calculateBordersJob.await()
 
             convolutedImage.copyInto(image)
         }
